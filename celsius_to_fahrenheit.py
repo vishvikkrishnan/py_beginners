b55f425a43406278988d121f3de355015c8aa55c
Lakshya Dhariwal <lakshyadhariwal9@gmail.com>
2020-10-15 12:14:11 +0530

    Create celsius_to_fahrenheit.py
    Pytnon code to convert temp in c to f

---
+++ celsius_to_fahrenheit.py
@@ -0,0 +1,7 @@
+# Python code to convert temperature inputed in celsius to fahrenheit
+
+temp_c = int(input("ENTER TEMPERATURE IN CELCIUS :"))
+
+temp_f = ((temp_c/5)* 9 ) +32
+
+print("TEMPERATURE IN FAHRENHEIT :", temp_c )
