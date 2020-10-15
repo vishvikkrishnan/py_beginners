b25751182d681e05e94c00b18c90350a036764e9
Lakshya Dhariwal <lakshyadhariwal9@gmail.com>
2020-10-15 12:16:33 +0530

    Create fahrenheit_to_celsius.py
    Python code to convert temperature inputed in  fahrenheit to celsius

---
+++ fahrenheit_to_celsius.py
@@ -0,0 +1,7 @@
+# Python code to convert temperature inputed in  fahrenheit to celsius 
+
+temp_f = int(input("ENTER TEMPERATURE IN FAHRENHEIT :"))
+
+temp_f = ((temp_c - 32)* 5 ) / 9
+
+print("TEMPERATURE IN CELSIUS :", temp_c )
