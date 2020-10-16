ac3b9987cbb67dcfd50824690ab4f804b5ee012d
Lakshya Dhariwal <lakshyadhariwal9@gmail.com>
2020-10-16 13:22:47 +0530

    Update reverse_integer.py

--- reverse_integer.py
+++ reverse_integer.py
@@ -1,9 +1,10 @@
-n= int(input('Enter Number to reverse'))
-i = n
-m  = 0
-while i>0:
-    m = m*10
-    m = m + i%10
-    i = i//10
+#this code will input a integer from user and print its reverse
+num = int(input('Enter Number to reverse'))
+counter = num
+rev_no = 0
+while counter > 0 :
+    rev_no = rev_no*10
+    rev_no = rev_no + counter % 10  # % is the mod operator which gives the remainder
+    counter = counter // 10   # // is the Floor division operator - division that results into whole number adjusted to the left in the number line
 
-print(m)
+print("reverse of the number", rev_no)   
