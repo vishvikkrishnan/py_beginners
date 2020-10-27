26d6de5ec263868bf6a2a80e297cc0ea949a713b
Charliiee <carlin.afonso@gmail.com>
2020-10-27 17:22:49 -0300

    Improve prime verification algorithm

--- prime.py
+++ prime.py
@@ -1,16 +1,24 @@
-print("Enter the number: ")
-num = int(input())
-if num > 1:
-   # check for factors
-   for i in range(2,num):
-       if (num % i) == 0:
-           print(num,"is not a prime number")
-           print(i,"times",num//i,"is",num)
-           break
-   else:
-       print(num,"is a prime number")
-       
-# if input number is less than
-# or equal to 1, it is not prime
+#!/usr/bin/env python
+
+def is_prime(number):
+    # check if number is greater than 1
+    if number <= 1:
+        return False
+    if number == 2:
+        return True
+    if number % 2 == 0:
+        return False
+
+    for i in range(3, 1 + int(number**0.5), 2):
+        if number % i == 0:
+            return False
+
+    return True
+
+
+number = int(input("Enter a number: "))
+
+if is_prime(number):
+    print(number, "is a prime number")
 else:
-   print(num,"is not a prime number")
+    print(number, "is not a prime number")
